4688a833d76858abe6717046aada05cd72568c0c
Иван Чмиль <chmil.ivan.u@gmail.com>
2023-01-17 15:14:37 +0200

    err fix

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './css/styles.css';
-import { fetchCountries } from './fetchCountries';
+import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 const DEBOUNCE_DELAY = 300;
